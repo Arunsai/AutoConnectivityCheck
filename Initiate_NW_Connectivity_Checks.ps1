628846219e6a1ef2de50816fa4d33a37f65bad35
Arun Panchagnula <arunsai195@gmail.com>
2024-03-03 23:10:53 +1100

    Fetch components dynamically
    Added capability to fetch the components dynamically

--- Initiate_NW_Connectivity_Checks.ps1
+++ Initiate_NW_Connectivity_Checks.ps1
@@ -1,45 +1,114 @@
+#Capabilities to Mature
+#1. Improve the authenticaton experience for Unix
+#2. Import all hosts from a CSV file and read it into a variable
+#3. Add more capabilities to the script like certificate removals, certificate expiry checks, software installations, etc.
+#4. Flatten a multi dimentional array using the instructions in https://stackoverflow.com/questions/711991/flatten-array-in-powershell
+
 $TargetType = "Windows"
 $TargetPort = "5985"
-$Input_File = "Telnet_Input.csv"
+$Failures = 0
+
+$Input_File = "Telnet_Input.csv"  #<---InputFile is harded coded to "Telnet_Input.csv" to ealisy cleanup the files from component servers after running the scripts.
 $SuccessParams = @{ NoNewLine = $false; ForegroundColor = 'Green' }
 $FailedParams = @{ NoNewLine = $false; ForegroundColor = 'Red' }
 $ProgressParams = @{ NoNewLine = $true; ForegroundColor = 'Blue' }
+$QueryParams = @{ NoNewLine = $false; ForegroundColor = 'Yellow' }
+
+
+$Computers = Import-csv "export.csv"
+$Hostname = ""
+$count = $x = $y = $z = 0
 
 #Declare CyberArk Component servers
-$M_CPM_EDN = @("wsapp15260","wsapp15263")
-#$M_CPM_EDN = @("wsapp15237","wsapp15260")
-$M_CPM_CMN = @("wsapp15238","wsapp15261")
-$CPM_OPNET = @("wsapp15239","wsapp15262")
+$M_CPM_EDN = @()
+$M_CPM_CMN = @()
+$M_CPM_OPNET = @()
 
-$M_PSM_EDN = @("wsapp15240","wsapp15263")
-$M_PSM_CMN = @("wsapp15241","wsapp15264")
-$M_PSM_OPNET = @("wsapp15242","wsapp15265")
+$M_PSM_EDN = @()
+$M_PSM_CMN = @()
+$M_PSM_OPNET = @()
 
-$M_PSMP_EDN = @("lxclpampsmpedn01","lxclpampsmpedn02")
-#$M_PSMP_EDN = @("lxclpampsmpedn01","lxexpamnppsmpedn01")
-$M_PSMP_CMN = @("lxclpampsmpcmn01","lxexpamnppsmpcmn01")
-$M_PSMP_OPNET = @("lxclpamnppsmpopnet01","lxexpamnppsmpopnet01")
+$M_PSMP_EDN = @()
+$M_PSMP_CMN = @()
+$M_PSMP_OPNET = @()
 
-$P_CPM_EDN = @("wsapp15223","wsapp15249")
-$P_CPM_CMN = @("wsapp15224","wsapp15250")
-$P_CPM_OPNET = @("wsapp15225","wsapp15251")
+$P_CPM_EDN = @()
+$P_CPM_CMN = @()
+$P_CPM_OPNET = @()
 
-$P_PSM_EDN = @("wsapp15226","wsapp15227","wsapp15252")
-$P_PSM_CMN = @("wsapp15228","wsapp15229","wsapp15253")
-$P_PSM_OPNET = @("wsapp15230","wsapp15231","wsapp15254")
+$P_PSM_EDN = @()
+$P_PSM_CMN = @()
+$P_PSM_OPNET = @()
 
-$P_PSMP_EDN = @("lxclpampsmpedn01","lxclpampsmpedn02","lxexpampsmpedn01")
-$P_PSMP_CMN = @("lxclpampsmpcmn01","lxclpampsmpcmn02","lxexpampsmpcmn01")
-$P_PSMP_OPNET = @("lxclpampsmpopnet01","lxclpampsmpopnet02","lxexpampsmpopnet01")
-
-#$AllHosts = @((("wsapp15237","wsapp15260"),("wsapp15238","wsapp15261"),("wsapp15239","wsapp15262")),(("wsapp15240","wsapp15263"),("wsapp15241","wsapp15264"),("wsapp15242","wsapp15265")),(("psmpEDN1","psmpEDN2"),("psmpCMN1","psmpCMN2"),("psmpOPNET1","psmpOPNET2")))
-#$AllHosts = @((("wsapp15260","wsapp15263"),("wsapp15238","wsapp15261"),("wsapp15239","wsapp15262")),(("wsapp15240","wsapp15263"),("wsapp15241","wsapp15264"),("wsapp15242","wsapp15265")),(("lxclpampsmpedn01","lxclpampsmpedn02","lxexpampsmpedn01"),("psmpCMN1","psmpCMN2"),("psmpOPNET1","psmpOPNET2")))
-#$AllHosts = @((("wsapp15230","wsapp15231"),("wsapp15238","wsapp15261"),("wsapp15239","wsapp15262")),(("wsapp15240","wsapp15263"),("wsapp15241","wsapp15264"),("wsapp15242","wsapp15265")),(("lxclpampsmpedn01","lxclpampsmpedn02","lxexpampsmpedn01"),("psmpCMN1","psmpCMN2"),("psmpOPNET1","psmpOPNET2")))
+$P_PSMP_EDN = @()
+$P_PSMP_CMN = @()
+$P_PSMP_OPNET = @()
 
 $All_M_Hosts = @(($M_CPM_EDN,$M_CPM_CMN,$M_CPM_OPNET),($M_PSM_EDN,$M_PSM_CMN,$M_PSM_OPNET),($M_PSMP_EDN,$M_PSMP_CMN,$M_PSMP_OPNET))
 $All_P_Hosts = @(($P_CPM_EDN,$P_CPM_CMN,$P_CPM_OPNET),($P_PSM_EDN,$P_PSM_CMN,$P_PSM_OPNET),($P_PSMP_EDN,$P_PSMP_CMN,$P_PSMP_OPNET))
-
 $AllHosts = @($All_M_Hosts,$All_P_Hosts)
+
+
+foreach ($computer in $Computers)
+{
+    $x = $y = $z = -1
+    #Write-Host("$x - $y - $z")
+	if($computer.Environment -eq "Model"){
+		$x=0
+	}
+	if($computer.Environment -eq "Production"){
+		$x=1
+	}
+
+	if($computer.Component -eq "CPM"){
+		$y=0
+	}
+	if($computer.Component -eq "PSM"){
+		$y=1
+	}
+	if($computer.Component -eq "PSMP"){
+		$y=2
+	}
+
+	if($computer.Network -eq "EDN"){
+		$z=0
+	}
+	if($computer.Network -eq "CMN"){
+		$z=1
+	}
+	if($computer.Network -eq "OPNET"){
+		$z=2
+	}
+
+    $count += 1
+    if (($x -ge 0) -and ($y -ge 0) -and ($z -ge 0)){
+        $Hostname = $computer.Hostname
+        $AllHosts[$x][$y][$z] += $Hostname
+    }
+#    if (($x -eq 1) -and ($y -eq 2) -and ($z -eq 2)){
+#        $Hostname = $computer.Hostname
+#        Write-Output "$x-$y-$z ..$Hostname"
+#    }
+}
+#$targets = $AllHosts[1][2][2]
+#Write-Output "Prod PSMP OPNET: $targets"
+#exit
+
+# Functions section
+function PerformDnsLookup {
+    param (
+        [string]$hostname
+    )
+
+    try {
+        $ipAddress = [System.Net.Dns]::GetHostAddresses($hostname)[0].IPAddressToString
+        # Write-Output "DNS Lookup successful. IP address: $ipAddress"
+        $ipAddress
+    } catch {
+        Write-Output "$thisHost,$target,DNSFAIL,,"
+        exit 1
+    }
+}
 
 #Input Validation section
 $EnvInputValidation = {
@@ -113,15 +182,18 @@ $NetworkInputValidation = {
         & $NetworkInputValidation
     }
 }
-
 #End Input Validation section
 
 #Dynamically prompt for Inputs that are required to run the script:
-Write-Host "Select the environment`n1. Model`n2. Production";
+Write-Host @QueryParams "Select the environment`n1. Model`n2. Production";
 $Env = & $EnvInputValidation
 $Env -=1
 
-Write-Host "Select the Component`n1. CPM`n2. PSM`n3. PSMP";
+if($Env -eq 0){
+$Domain="TEST-ACCOUNT-01\"
+}
+
+Write-Host @QueryParams "Select the Component`n1. CPM`n2. PSM`n3. PSMP";
 $Target_Component = & $ComponentInputValidation
 if($Target_Component -eq 3){
 	$TargetType = "Unix"
@@ -133,13 +205,11 @@ else{
 }
 $Target_Component -=1
 
-Write-Host "Select the Network`n1. EDN`n2. CMN`n3. OPNET";
+Write-Host @QueryParams "Select the Network`n1. EDN`n2. CMN`n3. OPNET";
 $Target_Network = & $NetworkInputValidation
 $Target_Network -=1
 
 $Script = Read-Host -Prompt 'Specify the script to run:'
-#$Script = "check_connectivity.sh"
-#Input gathhering completed
 
 #Retrieve the component servers based on the inputs:
 $targets = $AllHosts[$Env][$Target_Component][$Target_Network]
@@ -147,22 +217,29 @@ $targets = $AllHosts[$Env][$Target_Component][$Target_Network]
 #Display selected hosts based on the inputs provided
 if ($targets.count -le 0){
 	Write-Host @FailedParams "No Hosts detected with these options.. Exiting..";
-exit
+    exit
 }
 else {
-	Write-Host @SuccessParams "Selected component servers are: $targets ";
+    Write-Host @SuccessParams "Selected component servers are: $targets ";
 }
 
 if($TargetType -eq "Unix"){
 $UnixUser = Read-Host -Prompt 'Provide the Unix user name:'
 
 foreach ($Comp in $targets){
-$connection = Test-NetConnection -ComputerName $Comp -Port $TargetPort
+
+if ($Comp -as [ipaddress]) {
+    $ipAddress = $Comp
+} else {
+    $ipAddress = PerformDnsLookup -hostname $Comp
+}
+
+$connection = Test-NetConnection -ComputerName $ipAddress -Port $TargetPort
 
 if ($connection.TcpTestSucceeded) {
 	Write-Host "Connectivity exists to $Comp over Port $TargetPort. Creating a session to the host $Comp";
 	$DestFile = $Comp+"_Telnet_Result.csv"
-	$Session = New-PSSession -HostName $Comp -UserName $UnixUser
+	$Session = New-PSSession -HostName $ipAddress -UserName $UnixUser
 
 	#Copy the files to the remote host
 	Write-Host @FailedParams "Copying the script to $Comp..";
@@ -172,14 +249,15 @@ if ($connection.TcpTestSucceeded) {
 
 	Write-Host "ENTERING THE SESSION to $Comp..";
 	Enter-PSSession $Session
-	Write-Host @SuccessParams "Inside the session of $Comp..";
-	Invoke-Command -Session $Session {chmod +x check_connectivity.sh}
-	Invoke-Command -Session $Session {./check_connectivity.sh}
+	#Write-Host @SuccessParams "Inside the session of $Comp..";
+	#Invoke-Command -Session $Session {ls -l}
+	Invoke-Command -Session $Session {chmod +x Telnet_Unix_connectivity.sh}
+	Invoke-Command -Session $Session {./Telnet_Unix_connectivity.sh}
 	Exit-PSSession
 	Copy-Item "\home\$UnixUser\Telnet_Result.csv" -Destination ".\$DestFile" -FromSession $Session
 	Write-Host @SuccessParams "Successfully copied the results from $Comp";
 	Enter-PSSession $Session
-	Invoke-Command -Session $Session {rm -f check_connectivity.sh Telnet_Input.csv Telnet_Result.csv}
+	Invoke-Command -Session $Session {rm -f Telnet_Unix_connectivity.sh Telnet_Input.csv Telnet_Result.csv}
 	Exit-PSSession
 
 	#copy the hosts.txt file
@@ -194,22 +272,32 @@ if ($connection.TcpTestSucceeded) {
 }
 else {
 	Write-Host @FailedParams "No Connectivity to $Comp over Port $TargetPort. Skipping the Checks from the host $Comp";
+    $Failures += 1
 }
 
 }
 }
 
 else {
-$User = whoami
+#$User = whoami
 $Creds = Get-Credential
 
 Write-Host "Running the script on $targets";
 foreach ($Comp in $targets){
-$connection = Test-NetConnection -ComputerName $Comp -Port $TargetPort
+
+if ($Comp -as [ipaddress]) {
+    $ipAddress = $Comp
+} else {
+    $ipAddress = PerformDnsLookup -hostname $Comp
+}
+
+
+$connection = Test-NetConnection -ComputerName $ipAddress -Port $TargetPort
 
 if ($connection.TcpTestSucceeded) {
 	Write-Host "Connectivity exists to $Comp over Port $TargetPort. Creating a session to the host $Comp";
 	$DestFile = $Comp+"_Telnet_Result.csv"
+    #Set-Item WSMan:\\localhost\client\TrustedHosts -Value $ipAddress -Concatenate -Force
 	$Session = New-PSSession -ComputerName $Comp -Credential $Creds
 	Write-Host "Copying the input file to $Comp..";
 	Copy-Item $Input_File -Destination "C:\Users\$env:username\Documents\" -ToSession $Session
@@ -221,7 +309,13 @@ if ($connection.TcpTestSucceeded) {
 }
 else {
 	Write-Host @FailedParams "No Connectivity to $Comp over Port $TargetPort. Skipping the Checks from the host $Comp";
+    $Failures +=1
 }
 }
 }
-Write-Host @SuccessParams "Script execution completed..";
+if ($Failures -gt 0){
+Write-Host @FailedParams "Script execution completed..with errors";
+}
+else {
+Write-Host @SuccessParams "Script execution completed..successfully";
+}
